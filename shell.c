3738efdbd3aa077654191a1613845bd98b4720ed
othmane <othma125@hotmail.com>
2023-05-07 15:54:24 +0100

    fixing seg fault

--- shell.c
+++ shell.c
@@ -2,21 +2,14 @@
 
 /**
  * read_line - read command line
+ * @input: input command line
  * Return: command decomposition
  */
-char **read_line(void)
+char **read_line(char *input)
 {
-	size_t len;
-	int argc, i;
-	char *input = NULL, *input_cpy = NULL;
-	char *delim = " \n", *token = NULL, **args = NULL;
+	char *input_cpy = NULL;
+	char *delim = " \n";
 
-	getline(&input, &len, stdin);
-	if (len == 0)
-	{
-		printf("Shell has left the chat\n");
-		return (NULL);
-	}
 	input_cpy = malloc(sizeof(char) * (strlen(input) + 1));
 	if (!input_cpy)
 	{
@@ -24,27 +17,7 @@ char **read_line(void)
 		return (NULL);
 	}
 	strcpy(input_cpy, input);
-	free(input);
-	argc = 0;
-	do {
-		token = strtok(argc == 0 ? input_cpy : NULL, delim);
-		argc++;
-	} while (token);
-	args = malloc(sizeof(char *) * (1 + argc));
-	if (args == NULL)
-	{
-		printf("Womp womp woomp, sorry, no can do!");
-		return (NULL);
-	}
-	for (i = 0; i < argc; i++)
-	{
-		token = strtok(i == 0 ? input_cpy : NULL, delim);
-		printf("%s ", token);
-		args[i] = malloc(sizeof(char) * (strlen(token) + 1));
-		strcpy(args[i], token);
-	}
-	args[i] = NULL;
-	free(input_cpy);
+	return (_strtok(input_cpy, delim));
 }
 /**
  * main - simple shell program
@@ -55,15 +28,24 @@ int main(void)
 	char **args = NULL;
 	pid_t pid;
 	int status;
+	size_t n = 0;
+	char *input = NULL;
 
 	while (1)
 	{
 		printf("$ ");
 		if (args != NULL)
 			free(args);
-		args = read_line();
+		if (getline(&input, &n, stdin) == -1)
+		{
+			printf("Shell has left the chat\n");
+			free(input);
+			return (0);
+		}
+		printf("no error");
+		args = read_line(input);
 		if (args == NULL)
-		  return (-1);
+			return (-1);
 		pid = fork();
 		if (pid == 0)
 		{
--- strtok.c
+++ strtok.c
@@ -1,13 +1,13 @@
 #include "head.h"
 /**
- * exits_within - check code
+ * exists_within - check code
  * @c: character
  * @s: string
  * Return: boolean
  */
-int exits_within(char c, char *s)
+int exists_within(char c, char *s)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; i < strlen(s); i++)
 	{
@@ -25,14 +25,14 @@ int exits_within(char c, char *s)
 char **_strtok(char *s, char *delim)
 {
 	char **tokens;
-	size_t len = strlen(s);
-	int i, j = 0, k = 0, found = 0;
+	size_t i, len = strlen(s);
+	int j = 0, k = 0, found = 0;
 
 	tokens = (char **)malloc(len * sizeof(char));
 	*tokens = (char *)malloc(len * sizeof(char));
 	for (i = 0; i < len; i++)
 	{
-		if (exists_within(s + i, delim))
+		if (exists_within(s[i], delim))
 		{
 			if (found == 0)
 			{
@@ -42,6 +42,7 @@ char **_strtok(char *s, char *delim)
 			tokens[k] = (char *)malloc(len * sizeof(char));
 			found = 1;
 			j = 0;
+			printf("\n");
 		}
 		else
 		{
