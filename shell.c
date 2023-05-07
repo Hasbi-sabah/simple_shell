42cf8aa4548110045f31c85f82a2d07b82fb00bb
othmane <othma125@hotmail.com>
2023-05-07 12:06:23 +0100

    read line function

--- shell.c
+++ shell.c
@@ -1,58 +1,70 @@
 #include "head.h"
 
 /**
+ * read_line - read command line
+ * Return: command decomposition
+ */
+char **read_line(void)
+{
+	size_t len;
+	int argc, i;
+	char *input = NULL, *input_cpy = NULL;
+	char *delim = " \n", *token = NULL, **args = NULL;
+
+	getline(&input, &len, stdin);
+	if (len == 0)
+	{
+		printf("Shell has left the chat\n");
+		return (NULL);
+	}
+	input_cpy = malloc(sizeof(char) * (strlen(input) + 1));
+	if (!input_cpy)
+	{
+		printf("Womp womp woomp, sorry, no can do!");
+		return (NULL);
+	}
+	strcpy(input_cpy, input);
+	free(input);
+	argc = 0;
+	do {
+		token = strtok(argc == 0 ? input_cpy : NULL, delim);
+		argc++;
+	} while (token);
+	args = malloc(sizeof(char *) * (1 + argc));
+	if (args == NULL)
+	{
+		printf("Womp womp woomp, sorry, no can do!");
+		return (NULL);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		token = strtok(i == 0 ? input_cpy : NULL, delim);
+		printf("%s ", token);
+		args[i] = malloc(sizeof(char) * (strlen(token) + 1));
+		strcpy(args[i], token);
+	}
+	args[i] = NULL;
+	free(input_cpy);
+}
+/**
  * main - simple shell program
  * Return: 0
  */
-
 int main(void)
 {
-	char *input = NULL, *input_cpy = NULL, *delim = " \n", *token = NULL, **args;
-	size_t n, i;
-	int argc;
+	char **args = NULL;
 
 	while (1)
 	{
 		printf("$ ");
-		n = getline(&input, &n, stdin);
-		if ((int)n == -1)
-		{
-			printf("Shell has left the chat\n");
+		if (args != NULL)
+			free(args);
+		args = read_line();
+		if (args == NULL)
 			return (-1);
-		}
-		input_cpy = malloc(sizeof(char) * (strlen(input) + 1));
-		if (!input_cpy)
-		{
-			printf("Womp womp woomp, sorry, no can do!");
-			return (-1);
-		}
-		strcpy(input_cpy, input);
-		argc = 0;
-		do {
-			token = strtok(argc == 0 ? input_cpy : NULL, delim);
-			argc++;
-		} while (token);
-		argc++;
-		args = malloc(sizeof(char *) * argc);
-		if (!args)
-		{
-			printf("Womp womp woomp, sorry, no can do!");
-			return (-1);
-		}
-		for (i = 0; i < argc; i++)
-		{
-			token = strtok(i == 0 ? input_cpy : NULL, delim);
-			printf("%s ", token);
-			args[i] = malloc(sizeof(char) * (strlen(token) + 1));
-			strcpy(args[i], token);
-		}
-		args[i] = NULL;
 		execmd(args);
 	}
 	printf("\n");
 	free(args);
-	free(token);
-	free(input_cpy);
-	free(input);
 	return (0);
 }
