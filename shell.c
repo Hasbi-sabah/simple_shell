060e34e49f7d83b4b0430e6c732d17ed5105b529
H. Sabah <sabahhasbi00@gmail.com>
2023-05-09 06:02:17 -0700

    handles echo VAR + minor fixes

--- shell.c
+++ shell.c
@@ -12,10 +12,7 @@ char **read_line(char *input)
 
 	input_cpy = malloc(sizeof(char) * (strlen(input) + 1));
 	if (!input_cpy)
-	{
-		printf("Womp womp woomp, sorry, no can do!");
 		return (NULL);
-	}
 	strcpy(input_cpy, input);
 	return (_strtok(input_cpy, delim));
 }
@@ -23,32 +20,34 @@ char **read_line(char *input)
  * main - simple shell program
  * Return: 0
  */
-int main(void)
+int main(int argc, char **args)
 {
-	char **args = NULL;
 	pid_t pid;
 	int status;
 	size_t n = 0;
-	char *input = NULL;
+	char *input = NULL, *name = args[0];
+
+	(void) argc;
 
 	while (1)
 	{
 		printf("$ ");
 		if (args != NULL)
-			free(args);
+			args = NULL;
 		if (getline(&input, &n, stdin) == -1)
 		{
-			printf("Shell has left the chat\n");
 			free(input);
 			return (0);
 		}
 		args = read_line(input);
 		if (args == NULL)
 			return (-1);
+		if (strcmp(args[0], "exit") == 0)
+			return (0);
 		pid = fork();
 		if (pid == 0)
 		{
-			execmd(args);
+			execmd(args, name);
 			exit(0);
 		}
 		else
