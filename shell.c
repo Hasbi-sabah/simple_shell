f80c9777142481896e03a9616b92d18d2c33d796
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-05-12 19:50:03 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -24,7 +24,10 @@ int main(int argc, char **args)
 	        arr = split_line(input);
 	        free(input);
 		if (arr == NULL)
+		{
+			write(1, "\n", 1);
 			continue;
+		}
 		printf("%d\n", args_count(arr));
 	        if (args_count(arr) > 0 && cmd_selector(arr[0], arr) == 0)
 			_fork(name, arr);
