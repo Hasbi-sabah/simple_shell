289b295a7b78daa37c73b0cc6f0416fda1ace6b6
othmane <othma125@hotmail.com>
2023-05-12 19:16:16 +0100

    change dir

--- change_dir.c
+++ change_dir.c
@@ -9,10 +9,17 @@
 void change_dir(int argc, char **args)
 {
 	int r;
-	char *path;
+	char *path, *error;
 
 	path = argc == 1 ? "~" : args[1];
 	r = chdir(path);
 	if (r < 0)
-		perror("cd: %s: No such file or directory", path);
+	{
+		error = malloc(strlen(path) + 4 + 29);
+		strcpy(error, "cd: ");
+		strcat(error, path);
+		strcat(error, ": No such file or directory\n");
+		write(2, error, strlen(error));
+		free(error);
+	}
 }
