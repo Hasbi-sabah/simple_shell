ccb3b6134ec30f915f1f04c33f6b3da774897d92
othmane <othma125@hotmail.com>
2023-05-13 16:31:25 +0100

    change dir update

--- change_dir.c
+++ change_dir.c
@@ -6,12 +6,15 @@
  * @args: arguments
  * Return: none
  */
+char previous[1024];
 void change_dir(int argc, char **args)
 {
 	int r;
 	char *path, *error;
 
 	path = argc == 1 || strcmp(args[1], "~") == 0 ? getenv("HOME") : args[1];
+	if (strcmp(path, "-") == 0)
+		path = previous;
 	r = chdir(path);
 	if (r < 0)
 	{
@@ -23,5 +26,6 @@ void change_dir(int argc, char **args)
 		free(error);
 		return;
 	}
+	getcwd(previous, sizeof(previous));
 	setenv("PWD", path, 1);
 }
