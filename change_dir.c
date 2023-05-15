f3be428fc21bbd581d3aed47f3620b4141242632
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-05-15 21:11:45 +0100

    replace getenv by _getenv

--- change_dir.c
+++ change_dir.c
@@ -14,7 +14,7 @@ void change_dir(int argc, char **args)
 
 	if (!previous)
 		getcwd(previous, 1024);
-	path = argc == 1 || strcmp(args[1], "~") == 0 ? getenv("HOME") : args[1];
+	path = argc == 1 || strcmp(args[1], "~") == 0 ? _getenv("HOME") : args[1];
 	if (strcmp(path, "-") == 0)
 		path = previous;
 	r = chdir(path);
