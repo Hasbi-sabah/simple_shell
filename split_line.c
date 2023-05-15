e73c6fca2d8b319c6cdc165c7f87542b016e1c2e
othmane <othma125@hotmail.com>
2023-05-15 22:31:10 +0100

    replce function

--- split_line.c
+++ split_line.c
@@ -20,7 +20,23 @@ void replace_substring(char* str, const char* old_substring, const char* new_sub
  * split_line - read command line
  * @input: input command line
  * Return: command decomposition
- */
+ *
 char **split_line(char *input)
 {
+	size_t old_len = strlen(input);
+
+	replace_substring(input, "&&", "&");
+	if (strlen(input) < old_len)
+		return (_strtok(input, "&"));
+	replace_substring(input, "||", "|");
+	if (strlen(input) < old_len)
+		return (_strtok(input, "|"));
+	return (&input);
+}
+*/
+int main() {
+  char str[] = "the quick red fox jumps over the lazy dog";
+  replace_substring(str, "red", "brown");
+  printf("%s\n", str); // Output: "the quick red fox jumps over the lazy dog"
+  return 0;
 }
