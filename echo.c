7aa5af9888c24de234007f629144454ef70f4cd5
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-05-15 18:14:48 +0100

    Update echo.c

--- echo.c
+++ echo.c
@@ -11,12 +11,12 @@ int echo(int argc, char **args)
 {
 	if (argc == 2)
 	{
-		if (strcmp(args[1], "$$"))
+		if (!strcmp(args[1], "$$"))
 		{
 			printf("%d\n", getpid());
 			return (1);
 		}
-		else if (strcmp(args[1], "$?"))
+		else if (!strcmp(args[1], "$?"))
 		{
 			printf("%d\n", WEXITSTATUS(EXIT_STATUS));
 			return (1);
