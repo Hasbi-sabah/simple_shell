352e4ee5ed0d2a31073ed9e33d524a496af572cb
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-05-15 21:06:29 +0100

    Update split_line.c

--- split_line.c
+++ split_line.c
@@ -7,12 +7,4 @@
  */
 char **split_line(char *input)
 {
-	char *input_cpy = NULL;
-
-	if (input == NULL || strlen(input))
-		return (NULL);
-	input_cpy = strdup(input);
-	if (!input_cpy)
-		return (NULL);
-	return (_strtok(input_cpy, " \n"));
 }
