efdd9e79483959396358d4cf229f166fdb7b1c38
H. Sabah <sabahhasbi00@gmail.com>
2023-05-24 05:15:59 -0700

    betty, the traitor

--- str_funcs1.c
+++ str_funcs1.c
@@ -66,6 +66,11 @@ char *_strcat(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
+/**
+ * del - deletes the first character of a string
+ * @s: string
+ */
 void del(char **s)
 {
 	int i;
